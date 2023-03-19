0ee3d2c2b58e4619872796ea4a57b62b34168b53
Evellyn Rodrigues <evellynrocha489@gmail.com>
2023-03-19 19:56:41 -0300

    Alteração de 5 listas para apenas uma lista encadeada e melhoramente na função de remover

--- Double_list.py
+++ Double_list.py
@@ -1,84 +1,89 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+class Book:
+    def __init__(self, nome, autor, publicacao, genero, ibsn):
+        self.nome = nome
+        self.autor = autor
+        self.publicacao = publicacao
+        self.genero = genero
+        self.ibsn = ibsn
         self.previous = None
+        self.next = None
 
 class DoubleLinkedList:
     def __init__(self):
         self.head = None
+        self.previous = None
         self.size = 0
 
-    def append(self, elemento): 
-        new_node = Node(elemento)
+    def append(self, nome, autor, publicacao, genero, ibsn):
+        new_book = Book(nome, autor, publicacao, genero, ibsn)
 
         if self.head == None:
-            self.head = new_node
+            self.head = new_book
+            self.previous = new_book
         else:
-            pointer = self.head
-            while pointer.next != None:
-                pointer = pointer.next
-            pointer.next = new_node
-            new_node.previous = pointer
-            new_node.next = None
+            new_book.next = self.head
+            self.head.previous = new_book
+            self.head = new_book
         self.size += 1
 
-    def remove(self, index: int): 
-        if (index == 0):
-            pointer = self.head
-            if self.head.next != None:
-                self.head = self.head.next
-                self.head.previous = None
+    def remove(self, livro):
+        pointer = self.head
+        encontrado = False
+
+        while pointer != None and not encontrado:
+            if pointer.nome == livro:
+                encontrado = True
             else:
-                self.head = None
+                pointer = pointer.next
 
-            return pointer
-
-        position = 1
-        previous = self.head
-        pointer = self.head.next
-        while (position < index):
-            previous = pointer
-            pointer = pointer.next
-            position = position + 1
-        
-        if (pointer.next != None):
-            previous.next = pointer.next
-            pointer.next.previous = pointer.previous
+        if pointer == None:
+            print ("Livro não encontrado na biblioteca.")
+            
         else:
-            previous.next = None
-            pointer.previous = None
+            if pointer == self.head:
+                self.head = pointer.next
+                if self.head != None:
+                    self.head.previous = None
+                else:
+                    self.previous = None
 
-        self.size -= 1
-        return pointer
-        
+            elif pointer == self.previous:
+                self.previous = pointer.previous
+                self.previous.next = None
+
+            else:
+                pointer.previous.next = pointer.next
+                pointer.next.previous = pointer.previous
+
+            print(f'Livro "{livro} removido com sucesso!')
+            self.size -= 1
+
     def __len__(self):
         return self.size
     
-    def __getitem__(self, index):
+    def search(self, livro):
         pointer = self.head
-        
-        for i in range(index):
-            if pointer != None:
+        encontrado = False
+        while pointer != None and not encontrado:
+            if pointer.nome == livro:
+                encontrado = True
+            else:
                 pointer = pointer.next
-        return pointer.data
-
-    def index(self, elemento):
-        pointer = self.head
-        index = 0
-
-        while pointer != None:
-            if pointer.data == elemento:
-                return index
-            pointer = pointer.next
-            index += 1
+        if pointer == None:
+            print("Livro não encontrado na biblioteca.")
+        else:
+            print("Nome do livro: " + pointer.nome)
+            print("Autor: " + pointer.autor)
+            print("Data de publicação: " + str(pointer.publicacao))
+            print('Gênero: ' + str(pointer.genero))
+            print('IBSN: ' + str(pointer.ibsn))
     
     def __repr__(self):
         r = ''
         pointer = self.head
 
         while pointer != None:
-            r += str(pointer.data) + '\n'
+            r += f'Nome do livro: {str(pointer.nome)} | Autor: {str(pointer.autor)} | Ano de publicação: {str(pointer.publicacao)} | Gênero: {str(pointer.genero)} | ISBN: {str(pointer.ibsn)}' + '\n'
             pointer = pointer.next
         return r
     
